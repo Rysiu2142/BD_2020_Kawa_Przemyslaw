55a8c1535ab794ca8295bb14b2c8bb2ba1dbf11b
Rysiu2142 <45450164+Rysiu2142@users.noreply.github.com>
2020-05-15 15:30:45 +0200

    szablon strony

--- gui/index.php
+++ gui/index.php
@@ -1,24 +1,84 @@
-<?php
-session_start();
 
-$conn = oci_connect('student', 'student', 'localhost:1521/KSIAZKIFILMY');
-if (!$conn) {
-    $m = oci_error();
-    trigger_error(htmlentities($m['message']), E_USER_ERROR);
-}
-else{
-    echo("MAMA MIJA!");
-}
-
-?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TESTING</title>
-</head>
-<body>
-   
-</body>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>BuldoRex</title>
+		<meta name="description" content="Wypożyczalnia pojazdów budowlanych">
+		<meta name="keywords" content="wypożyczalnia pojazdów budowlanych, budowa, koparka">
+		<meta name="author" content="Przemek">
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	
+	<body>
+	<section id="start">
+	 	<div class="container text-center">
+		 <h1>BuldoRex</h1>
+		 <div class="logo"> <img src="img/logo.jpg"></div>
+		</div>
+	</section>
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container">
+				<ul class="nav justify-content-end">
+					<li class="nav-item">
+						<a class="nav-link active" href="index.php#start">Start</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php#onas">O nas</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php#onas#galeria">Galeria</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="index.php#onas#kontakt">Kontakt</a>
+					</li>
+				</ul>
+				<div class="my-2">
+				
+				</div>
+			</div>
+
+	</nav>
+	<section id="onas" class="bg-secondary">
+		<div class="container">
+		<h3 class="text-cenetr">O Nas</h3>
+			<p>W roku 1990 system GNU posiadał już edytor tekstu (Emacs),
+				kompilator (GCC) oraz większość podstawowych bibliotek i narzędzi standardowej dystrybucji Uniksa.
+				Jako że głównym założeniem było stworzenie w pełni wolnego systemu operacyjnego, a nie napisanie go zupełnie od nowa,
+				starano się używać wolnego oprogramowania wszędzie, gdzie było to możliwe. W latach 80. 
+				nie było go zbyt wiele; skorzystano więc z okienkowego systemu graficznego X Window System, systemu profesjonalnego składu drukarskiego
+				TeX i mikrojądra Mach i włączono je do GNU.
+			</p>
+		</div>
+	</section>
+	<section id="Galeria" class="bg-white">
+		<div class="container">
+		<h3 class="text-cenetr">Galeria</h3>
+			<p>W roku 1990 system GNU posiadał już edytor tekstu (Emacs),
+				kompilator (GCC) oraz większość podstawowych bibliotek i narzędzi standardowej dystrybucji Uniksa.
+				Jako że głównym założeniem było stworzenie w pełni wolnego systemu operacyjnego, a nie napisanie go zupełnie od nowa,
+				starano się używać wolnego oprogramowania wszędzie, gdzie było to możliwe. W latach 80. 
+				nie było go zbyt wiele; skorzystano więc z okienkowego systemu graficznego X Window System, systemu profesjonalnego składu drukarskiego
+				TeX i mikrojądra Mach i włączono je do GNU.
+			</p>
+		</div>
+	</section>
+	<section id="kontakt" class="bg-secondary">
+		<div class="container">
+		<h3 class="text-cenetr">Kontakt</h3>
+			<p>W roku 1990 system GNU posiadał już edytor tekstu (Emacs),
+				kompilator (GCC) oraz większość podstawowych bibliotek i narzędzi standardowej dystrybucji Uniksa.
+				Jako że głównym założeniem było stworzenie w pełni wolnego systemu operacyjnego, a nie napisanie go zupełnie od nowa,
+				starano się używać wolnego oprogramowania wszędzie, gdzie było to możliwe. W latach 80. 
+				nie było go zbyt wiele; skorzystano więc z okienkowego systemu graficznego X Window System, systemu profesjonalnego składu drukarskiego
+				TeX i mikrojądra Mach i włączono je do GNU.
+			</p>
+		</div>
+	</section>
+
+	</body>
 </html>
