9f03c34e70474f66a6ea0dcbb4e46c6720a34b13
Rysiu2142 <45450164+Rysiu2142@users.noreply.github.com>
2020-05-20 18:46:13 +0200

    Inserty i Selecty

--- sql/db_schema.sql
+++ sql/db_schema.sql
@@ -1,28 +1,4 @@
---DLL's for Sequences
-CREATE SEQUENCE Oceny_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
 
-CREATE SEQUENCE Ksiazki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-
-CREATE SEQUENCE Jezyki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-
-------------------------------------------------------------------------------------------------
--- DLL's for Sequence Autorzy-arm
-CREATE SEQUENCE Autorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-CREATE SEQUENCE KsiazkiAutorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-
-------------------------------------------------------------------------------------------------
--- DLL's for Sequence Gatunki-arm
-CREATE SEQUENCE Gatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-CREATE SEQUENCE KsiazkiGatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-
--------------------------------------------------------------------------------------------------
--- DDL's for Sequence film-side
-CREATE SEQUENCE Filmy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-CREATE SEQUENCE FilmyKategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-CREATE SEQUENCE Kategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
-CREATE SEQUENCE OcenyFilmu_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1  NOCACHE NOCYCLE;
-
---------------------------------------------------------------------------------------------------
 
 --DLL for Oceny
 CREATE TABLE Oceny (
@@ -37,16 +13,12 @@ CREATE TABLE Oceny (
 --DLL for Ksiazki
 CREATE TABLE Ksiazki (
   idksiazka NUMBER(10) PRIMARY KEY,
-  Tytul varchar2(255)
+  Tytul varchar2(255),
+  data_wydania date,
+  OrginalnyJezyk varchar2(255)
 );
 
---DLL for Jezyki
-CREATE TABLE Jezyki (
-  idjezyk NUMBER(10) PRIMARY KEY,
-  nazwa varchar2(255),
-  data_wydania date,
-  idksiazka NUMBER(10)
-);
+
 
 --DLL for Autorzy
 CREATE TABLE Autorzy (
@@ -105,211 +77,7 @@ CREATE TABLE FilmyKategoria (
   idkategoria NUMBER(10)
 );
 
------------------------------------------------------------------
--- DDL for Procedure CREATE (INSERT)
------------------------------------------------------------------
 
-CREATE OR REPLACE PROCEDURE CREATE_OCENA(
-  Ocena in OCENY.Ocena%TYPE,
-  Krytyk in OCENY.Krytyk%TYPE,
-  idksiazka in OCENY.idksiazka%TYPE
-  ) AS
-BEGIN
-  if( Ocena > 10 ) THEN
-        Ocena=10;
-  end if;
-  if( Ocena < 0 ) THEN
-        Ocena=0;
-  end if; 
-	INSERT INTO OCENY 
-	VALUES 
-	(
-	OCENY_SEQ.nextval,
-	Ocena,Krytyk,
-	idksiazka
-	);
-END CREATE_OCENA
-
------------------------------------------------------------------
-CREATE OR REPLACE PACKAGE T_GATUNEK AS
-
-FUNCTION gatunek_p(
-nazwa_1 in GATUNKI.Nazwa%TYPE
-) RETURN NUMBER;
-
-PROCEDURE CREATE_Gatunek
-(
- Nazwa in GATUNKI.Nazwa%TYPE
- );
-END T_GATUNEK;
-/
-
-CREATE OR REPLACE PACKAGE BODY T_GATUNEK AS
-
-FUNCTION gatunek_p(
-nazwa_1 in GATUNKI.Nazwa%TYPE
-) RETURN NUMBER IS
-  suma NUMBER:=0;
-  BEGIN
-    SELECT COUNT(*) INTO suma FROM GATUNKI WHERE Nazwa=nazwa_1;
-    RETURN suma;
-  END gatunek_p;
-  
-  PROCEDURE CREATE_Gatunek(
-  Nazwa in GATUNKI.Nazwa%TYPE
-  ) AS
-  BEGIN
-	INSERT INTO Gatunki
-	VALUES (
-	Gatunki_SEQ.nextval,
-	Nazwa);
-END CREATE_Gatunki
-END T_GATUNEK;
-	/
-  
-CREATE OR REPLACE PROCEDURE CREATE_KG
-(
- idgatunek in KsiazkiGatunki.idgatunek%TYPE
-, idksiazka in KsiazkiGatunki.idksiazka%TYPE
-) AS
-BEGIN
-	INSERT INTO KsiazkiGatunki
-	VALUES (
-	KsiazkiGatunki_SEQ.nextval,
-	idgatunek_in,
-	idksiazka_in);
-END CREATE_KG
-
-
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_Ksiazki
-(
- tytul_in in Ksiazki.Tytul%TYPE
-) AS
-BEGIN
-	INSERT INTO Ksiazki 
-	VALUES (
-	Ksiazki_SEQ.nextval,
-	tytul_in);
-END CREATE_Ksiazki
-
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_Jezyki
-(
- nazwa in  Jezyki.nazwa%TYPE,
- data_wyd in Jezyki.data_wydania%TYPE,
- idks in Jezyki.idksiazka%TYPE 
-) AS
-BEGIN
-	INSERT INTO Jezyki 
-	VALUES (
-	Jezyki_SEQ.nextval,
-	nazwa_in,
-	data_wyd,
-	idks);
-END CREATE_Jezyki
-
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_Autorzy
-(
- imie IN Autorzy.Imie%TYPE,
- nazwisko IN Autorzy.Nazwisko%TYPE,
- urodz IN Autorzy.urodzony%TYPE,
- zmarl IN Autorzy.zmarly%TYPE
-) AS
-BEGIN
-	INSERT INTO Autorzy (
-	VALUES (
-	Autorzy_SEQ.nextval,
-	imie,
-	nazwsko,
-	urodz,
-	zmarl);
-END CREATE_Autorzy
-
-CREATE OR REPLACE PROCEDURE CREATE_KA
-(
- idautor IN KsiazkiAutorzy.idautor%TYPE,
- idksiazka IN KsiazkiAutorzy.idksiazka%TYPE
-) AS
-BEGIN
-	INSERT INTO KsiazkiAutorzy
-	VALUES (
-	KsiazkiAutorzy_SEQ.nextval,
-	idautor,
-	idksiazka);
-END CREATE_KA
-
------------------------------------------------------------------
--- Fimy i pochodne
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_Filmy
-(
- tytul IN Filmy.Tytul%TYPE,
- idksiazka IN Filmy.idksiazka%TYPE
-) AS
-BEGIN
-	INSERT INTO Filmy 
-	VALUES (
-	Filmy_SEQ.nextval,
-	tytul,
-	ksiazka);
-END CREATE_Filmy
-
-
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_OcenyFilmu
-(
- ocena IN OcenyFilmu.Ocena%TYPE
- krytyk IN OcenyFilmu.Krytyk%TYPE
- idfilmu IN OcenyFilmu.idfilm%TYPE
-) AS
-BEGIN
-  if( ocena > 10 ) THEN
-        ocena=10;
-  end if;
-  if( ocena < 0 ) THEN
-        ocena=0;
-  end if; 
-	INSERT INTO OcenyFilmu 
-	VALUES (
-	OcenyFilmu_SEQ.nextval,
-	ocena,
-	krytyk,
-	idfilmu
-	);
-END CREATE_OcenyFilmu
-
------------------------------------------------------------------
-
-CREATE OR REPLACE PROCEDURE CREATE_Kategoria
-(
- Nazwa IN Kategoria.Nazwa%TYPE
-) AS
-BEGIN 
-	INSERT INTO Kategoria
-	VALUES (
-	Kategoria_SEQ.nextval,
-	Nazwa_in);
-END CREATE_Kategoria
-
-CREATE OR REPLACE PROCEDURE CREATE_FK
-(
- idfilm IN FilmyKategoria.idfilm%TYPE,
- idkategoria IN FilmyKategoria.idkategoria%TYPE
-) AS
-BEGIN
-	INSERT INTO FilmyKategoria
-	VALUES (
-	FilmyKategoria_SEQ.nextval,
-	idfilm,
-	idkategoria);
-END CREATE_KG
 
 
 ALTER TABLE Oceny ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
@@ -325,8 +93,6 @@ ALTER TABLE KsiazkiGatunki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksi
 ALTER TABLE FilmyKategoria ADD FOREIGN KEY (idfilm) REFERENCES Filmy (idfilm);
 
 ALTER TABLE FilmyKategoria ADD FOREIGN KEY (idkategoria) REFERENCES Kategoria (idkategoria);
-
-ALTER TABLE Jezyki ADD FOREIGN KEY (idksiazka) REFERENCES Ksiazki (idksiazka);
 
 ALTER TABLE OcenyFilmu ADD FOREIGN KEY (idocena) REFERENCES Filmy (idfilm);
 
---
+++ sql/plsql.sql
@@ -0,0 +1,408 @@
+--DLL's for Sequences
+CREATE SEQUENCE Oceny_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+CREATE SEQUENCE Ksiazki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+------------------------------------------------------------------------------------------------
+-- DLL's for Sequence Autorzy-arm
+CREATE SEQUENCE Autorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE KsiazkiAutorzy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+------------------------------------------------------------------------------------------------
+-- DLL's for Sequence Gatunki-arm
+CREATE SEQUENCE Gatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE KsiazkiGatunki_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+
+-------------------------------------------------------------------------------------------------
+-- DDL's for Sequence film-side
+CREATE SEQUENCE Filmy_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE FilmyKategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE Kategoria_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1 NOCACHE NOCYCLE;
+CREATE SEQUENCE OcenyFilmu_SEQ MINVALUE 1 MAXVALUE 999999 START with 1 INCREMENT BY 1  NOCACHE NOCYCLE;
+
+-----------------------------------------------------------------
+-- DDL for Procedure CREATE (INSERT)
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_OCENA AS
+
+CREATE OR REPLACE PROCEDURE CREATE_OCENA(
+  Ocena in OCENY.Ocena%TYPE,
+  Krytyk in OCENY.Krytyk%TYPE,
+  idksiazka in OCENY.idksiazka%TYPE
+  );
+END T_OCENA
+
+CREATE OR REPLACE PACKAGE BODY T_OCENA AS
+
+CREATE OR REPLACE PROCEDURE CREATE_OCENA(
+  Ocena in OCENY.Ocena%TYPE,
+  Krytyk in OCENY.Krytyk%TYPE,
+  idksiazka in OCENY.idksiazka%TYPE
+  ) AS
+BEGIN
+  if( Ocena > 10 ) THEN
+        Ocena=10;
+  end if;
+  if( Ocena < 0 ) THEN
+        Ocena=0;
+  end if; 
+	INSERT INTO OCENY 
+	VALUES 
+	(
+	OCENY_SEQ.nextval,
+	Ocena,Krytyk,
+	idksiazka
+	);
+END CREATE_OCENA;
+END T_OCENA;
+
+
+
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_GATUNEK AS
+
+FUNCTION gatunek_p(
+nazwa_1 in GATUNKI.Nazwa%TYPE
+) RETURN NUMBER;
+
+PROCEDURE CREATE_Gatunek
+(
+ Nazwa in GATUNKI.Nazwa%TYPE
+ );
+END T_GATUNEK;
+/
+
+CREATE OR REPLACE PACKAGE BODY T_GATUNEK AS
+
+FUNCTION gatunek_p(
+nazwa_1 in GATUNKI.Nazwa%TYPE
+) RETURN NUMBER IS
+  suma NUMBER:=0;
+  BEGIN
+    SELECT COUNT(*) INTO suma FROM GATUNKI WHERE Nazwa=nazwa_1;
+    RETURN suma;
+  END gatunek_p;
+  
+  PROCEDURE CREATE_Gatunek(
+  Nazwa in GATUNKI.Nazwa%TYPE
+  ) AS
+  BEGIN
+	INSERT INTO Gatunki
+	VALUES (
+	Gatunki_SEQ.nextval,
+	Nazwa);
+END CREATE_Gatunki
+END T_GATUNEK;
+	/
+	
+	CREATE OR REPLACE TRIGGER gatunek_male
+	BEFORE INSERT ON GATUNKI
+	FOR EACH ROW
+	BEGIN
+		:new.Nazwa:=lower(:new.Nazwa);
+	END;
+	/
+	ALTER TRIGGER gatunek_male ENABLE;
+	
+-----------------------------------------------------------------  
+CREATE OR REPLACE PACKAGE T_KG AS  
+    
+PROCEDURE CREATE_KG
+(
+ idgatunek in KsiazkiGatunki.idgatunek%TYPE
+, idksiazka in KsiazkiGatunki.idksiazka%TYPE
+);
+END T_KG;
+CREATE OR REPLACE PACKAGE BODY T_KG AS
+
+PROCEDURE CREATE_KG
+(
+ idgatunek in KsiazkiGatunki.idgatunek%TYPE
+, idksiazka in KsiazkiGatunki.idksiazka%TYPE
+)
+AS
+BEGIN
+	INSERT INTO KsiazkiGatunki
+	VALUES (
+	KsiazkiGatunki_SEQ.nextval,
+	idgatunek_in,
+	idksiazka_in);
+END CREATE_KG
+END T_KG;
+
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_Ksiazke AS  
+PROCEDURE CREATE_Ksiazki
+(
+ tytul_in in Ksiazki.Tytul%TYPE,
+ data_wydania in Ksiazki.data_wydania%TYPE,
+ OrginalnyJezyk in Ksiazki.OrginalnyJezyk%TYPE
+) ;
+END T_Ksiazke;
+
+CREATE OR REPLACE PACKAGE BODY T_Ksiazke AS  
+PROCEDURE CREATE_Ksiazki
+(
+ tytul_in in Ksiazki.Tytul%TYPE
+) AS
+BEGIN
+	INSERT INTO Ksiazki 
+	VALUES (
+	Ksiazki_SEQ.nextval,
+	tytul_in,
+	data_wydania,
+	OrginalnyJezyk
+	);
+END CREATE_Ksiazki
+END T_Ksiazke;
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_Autor AS
+FUNCTION autor_p(
+imie_1 in Autorzy.Imie%TYPE
+) RETURN NUMBER;
+
+PROCEDURE CREATE_Autorzy
+(
+ imie IN Autorzy.Imie%TYPE,
+ nazwisko IN Autorzy.Nazwisko%TYPE,
+ urodz IN Autorzy.urodzony%TYPE,
+ zmarl IN Autorzy.zmarly%TYPE
+);
+END T_Autor;
+
+CREATE OR REPLACE PACKAGE BODY T_Autor AS
+
+FUNCTION autor_p(
+imie_1 in Autorzy.Imie%TYPE
+) RETURN NUMBER IS
+	suma NUMBER :=0;
+BEGIN
+	SELECT COUNT(*) INTO suma FROM Autorzy WHERE Imie=imie_1;
+	RETURN suma;
+END autor_p;
+
+PROCEDURE CREATE_Autorzy
+(
+ imie IN Autorzy.Imie%TYPE,
+ nazwisko IN Autorzy.Nazwisko%TYPE,
+ urodz IN Autorzy.urodzony%TYPE,
+ zmarl IN Autorzy.zmarly%TYPE
+) AS
+BEGIN
+	INSERT INTO Autorzy (
+	VALUES (
+	Autorzy_SEQ.nextval,
+	imie,
+	nazwsko,
+	urodz,
+	zmarl);
+END CREATE_Autorzy;
+END T_Autor;
+
+CREATE OR REPLACE TRIGGER autor_male
+	BEFORE INSERT ON Autorzy
+	FOR EACH ROW
+	BEGIN
+		:new.Imie:=lower(:new.Imie);
+	END;
+	/
+	ALTER TRIGGER autor_male ENABLE;
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_KA AS
+PROCEDURE CREATE_KA
+(
+ idautor IN KsiazkiAutorzy.idautor%TYPE,
+ idksiazka IN KsiazkiAutorzy.idksiazka%TYPE
+);
+END T_KA;
+CREATE OR REPLACE PACKAGE BODY T_KA AS
+
+PROCEDURE CREATE_KA
+(
+ idautor IN KsiazkiAutorzy.idautor%TYPE,
+ idksiazka IN KsiazkiAutorzy.idksiazka%TYPE
+) AS
+BEGIN
+	INSERT INTO KsiazkiAutorzy
+	VALUES (
+	KsiazkiAutorzy_SEQ.nextval,
+	idautor,
+	idksiazka);
+END CREATE_KA;
+END T_KA;
+
+-----------------------------------------------------------------
+-- Fimy i pochodne
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_KA AS
+PROCEDURE CREATE_Filmy
+(
+ tytul IN Filmy.Tytul%TYPE,
+ idksiazka IN Filmy.idksiazka%TYPE
+);
+END T_KA;
+
+
+CREATE OR REPLACE PACKAGE BODY T_KA AS
+PROCEDURE CREATE_Filmy
+(
+ tytul IN Filmy.Tytul%TYPE,
+ idksiazka IN Filmy.idksiazka%TYPE
+) AS
+BEGIN
+	INSERT INTO Filmy 
+	VALUES (
+	Filmy_SEQ.nextval,
+	tytul,
+	ksiazka);
+END CREATE_Filmy;
+END T_KA;
+
+
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_OF AS
+
+PROCEDURE CREATE_OcenyFilmu
+(
+ ocena IN OcenyFilmu.Ocena%TYPE
+ krytyk IN OcenyFilmu.Krytyk%TYPE
+ idfilmu IN OcenyFilmu.idfilm%TYPE
+);
+END T_OF;
+
+CREATE OR REPLACE PACKAGE BODY T_OF AS
+
+PROCEDURE CREATE_OcenyFilmu
+(
+ ocena IN OcenyFilmu.Ocena%TYPE
+ krytyk IN OcenyFilmu.Krytyk%TYPE
+ idfilmu IN OcenyFilmu.idfilm%TYPE
+) AS
+BEGIN
+  if( ocena > 10 ) THEN
+        ocena=10;
+  end if;
+  if( ocena < 0 ) THEN
+        ocena=0;
+  end if; 
+	INSERT INTO OcenyFilmu 
+	VALUES (
+	OcenyFilmu_SEQ.nextval,
+	ocena,
+	krytyk,
+	idfilmu
+	);
+END CREATE_OcenyFilmu;
+END T_OF
+
+-----------------------------------------------------------------
+
+
+CREATE OR REPLACE PACKAGE T_Kategoria AS
+FUNCTION Kategoria_p(
+nazwa_1 in Kategoria.Nazwa%TYPE
+) RETURN NUMBER;
+
+PROCEDURE CREATE_Kategoria
+(
+ Nazwa IN Kategoria.Nazwa%TYPE
+);
+END T_Kategoria;
+
+CREATE OR REPLACE PACKAGE BODY T_Kategoria AS
+FUNCTION Kategoria_p(
+nazwa_1 in Kategoria.Nazwa%TYPE
+) RETURN NUMBER IS
+	suma NUMBER :=0;
+BEGIN
+	SELECT COUNT(*) INTO suma FROM Autorzy WHERE Nazwa=nazwa_1;
+	RETURN suma;
+END Kategoria_p;
+
+PROCEDURE CREATE_Kategoria
+(
+ Nazwa IN Kategoria.Nazwa%TYPE
+) AS
+BEGIN 
+	INSERT INTO Kategoria
+	VALUES (
+	Kategoria_SEQ.nextval,
+	Nazwa_in);
+END CREATE_Kategoria;
+END T_Kategoria;
+
+CREATE OR REPLACE TRIGGER Kategoria_male
+	BEFORE INSERT ON Kategoria
+	FOR EACH ROW
+	BEGIN
+		:new.Nazwa:=lower(:new.Nazwa);
+	END;
+	/
+	ALTER TRIGGER Kategoria_male ENABLE;
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE T_KF
+PROCEDURE CREATE_FK
+(
+ idfilm IN FilmyKategoria.idfilm%TYPE,
+ idkategoria IN FilmyKategoria.idkategoria%TYPE
+);
+END T_KF;
+ 
+CREATE OR REPLACE PACKAGE BODY T_KF
+PROCEDURE CREATE_FK
+(
+ idfilm IN FilmyKategoria.idfilm%TYPE,
+ idkategoria IN FilmyKategoria.idkategoria%TYPE
+) AS
+BEGIN
+	INSERT INTO FilmyKategoria
+	VALUES (
+	FilmyKategoria_SEQ.nextval,
+	idfilm,
+	idkategoria);
+END CREATE_KG;
+END T_KF;
+-----------------------------------------------------------------
+-- PLAIN SELECTS
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE S_OCENY AS
+PROCEDURE SE_OCENY(OCENY OUT SYS_REFCURSOR);
+END S_OCENY;
+
+CREATE OR REPLACE PACKAGE BODY S_OCENY AS
+PROCEDURE SE_OCENY(OCENY OUT SYS_REFCURSOR) AS
+BEGIN
+OPEN OCENY FOR
+SELECT idocena, Ocena, Krytyk FROM OCENY;
+END SE_OCENY;
+END S_OCENY;
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE F_Kategoria AS
+PROCEDURE Fet_Kategoria(
+idfilm in FilmyKategoria.idfilm%TYPE, Kategorie OUT SYS_REFCURSOR);
+END F_Kategoria;
+
+ CREATE OR REPLACE PACKAGE BODY F_Kategoria AS
+ PROCEDURE Fet_Kategoria(
+ idfilm_1 in FilmyKategoria.idfilm%TYPE,
+ Kategorie OUT SYS_REFCURSOR) AS
+ BEGIN
+	OPEN Kategorie FOR
+	SELECT k.Nazwa 
+	FROM  FilmyKategoria fk, Kategoria k
+	WHERE fk.idfilm=idfilm_1 AND fk.idkategoria=k.idkategoria;
+END Fet_Kategoria;
+END F_Kategoria;
+-----------------------------------------------------------------
+CREATE OR REPLACE PACKAGE F_Gatunki AS
+PROCEDURE Fet_Gatunki(idksiazka_1 in KsiazkiGatunki.idksiazka%TYPE,
+Gatunki_out OUT SYS_REFCURSOR) AS
+	BEGIN
+		OPEN Gatunki_out FOR
+		SELECT  g.Nazwa
+		FROM KsiazkiGatunki kg , GATUNKI g
+		WHERE idksiazka_1=kg.idksiazka AND kg.idgatunek=g.idgatunek;
+	END Fet_Gatunki;
+END F_Kategoria;
+ 
+
